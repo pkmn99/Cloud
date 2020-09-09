d4007ab06a5bff083b02792de5a05d733e9dbb62
Thinkpad T480 <pkmn99@gmail.com>
2020-09-09 10:44:17 +0800

    update retulst and change percentage calculation

--- code/plot_attribution.py
+++ code/plot_attribution.py
@@ -7,14 +7,23 @@ import cartopy.feature as cfeature
 #from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
 import matplotlib as mpl
 
-att5=xr.open_dataset('../data/results/xu/MODIS_attribu_new.nc')
-msgatt=xr.open_dataset('../data/results/xu/MSG_attribu_new.nc')
+#att5=xr.open_dataset('../data/results/xu/MODIS_attribu_new.nc')
+#msgatt=xr.open_dataset('../data/results/xu/MSG_attribu_new.nc')
+att5=xr.open_dataset('../data/results/xu/Modis_attribution0908.nc')
+msgatt=xr.open_dataset('../data/results/xu/MSG_attribution0908.nc')
 
 
 # Plot figure
 # MODIS Panel 
-#discmap5 = mpl.colors.ListedColormap(['red', 'blue','yellow', 'lime','tab:pink'])
-discmap5 = mpl.colors.ListedColormap(['blue','red','lime','yellow','tab:pink'])
+discmap5 = mpl.colors.ListedColormap(['red', 'blue','yellow', 'lime','tab:pink'])
+#discmap5 = mpl.colors.ListedColormap(['blue','red','lime','yellow','tab:pink'])
+
+v1=np.round((att5.attribution==1).sum()/(att5.attribution>0).sum()*100,0)
+v2=np.round((att5.attribution==2).sum()/(att5.attribution>0).sum()*100,0)
+v3=np.round((att5.attribution==3).sum()/(att5.attribution>0).sum()*100,0)
+v4=np.round((att5.attribution==4).sum()/(att5.attribution>0).sum()*100,0)
+v5=np.round((att5.attribution==5).sum()/(att5.attribution>0).sum()*100,0)
+
 fig = plt.figure(figsize=[10,5])
 pos1 = [0.05, 0.1, 1, 1] # [left, bottom, width, height]
 ax1 = fig.add_axes(pos1, projection=ccrs.PlateCarree())
@@ -29,10 +38,10 @@ cax1 = fig.add_axes(cbar1_pos)
 cb1 = mpl.colorbar.ColorbarBase(ax=cax1, cmap=discmap5, norm=Normalize(vmin=1, vmax=5) ,
                                         orientation='vertical', ticks=np.arange(1+4/10, 5.6, 4/5))
 
-cb1.ax.set_yticklabels(['Tree+\n(43%)','Tree$-$\n(23%)','Orography+\n(16%)','Orography$-$\n(11%)',
-                        'Others\n(8%)'], fontsize=10)
+cb1.ax.set_yticklabels(['Tree+\n(%d%%)'%v1,'Tree$-$\n(%d%%)'%v2,'Orography+\n(%d%%)'%v3,'Orography$-$\n(%d%%)'%v4,
+                        'Others\n(%d%%)'%v5], fontsize=10)
 cb1.ax.invert_yaxis()
-ax1.set_title('Attribution of potential cloud impact of forest (MODIS)')
+ax1.set_title('Attribution of potential cloud effect of forest (MODIS)',fontsize=14)
 
 
 # MSG Panel 
@@ -44,6 +53,6 @@ ax2.coastlines()
 ax2.set_title('MSG')
 
 #plt.savefig('../figure/figure_attribution.pdf',bbox_inches='tight')
-plt.savefig('../figure/figure_attribution_0525.png',dpi=300,bbox_inches='tight')
+plt.savefig('../figure/figure_attribution_0908.png',dpi=300,bbox_inches='tight')
 
 print('Figure saved')
